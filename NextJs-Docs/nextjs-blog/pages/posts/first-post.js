659abb69788b5c98a496447b92ff909721c1d711
Snehdeep <snehdeepsingh221199@gmail.com>
2021-12-03 23:52:22 +0530

    navigation between pages complete

--- NextJs-Docs/nextjs-blog/pages/posts/first-post.js
+++ NextJs-Docs/nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,12 @@
+import Link from 'next/link'
 export default function FirstPost() {
-    return <h1>First Post</h1>
+    return (
+        <>
+            <h1>First Post</h1>
+            <Link href="/"><a>Go Back !</a></Link>
+            {/* Note: If you need to link to an external page outside the Next.js app, just use an <a> tag without Link.
+            If you need to add attributes like, for example, className, add it to the a tag, not to the Link tag. */}
+        </>
+
+    )
 }
