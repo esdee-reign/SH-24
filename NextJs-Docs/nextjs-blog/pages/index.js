f5eb9d5820061b391d5647645ff05737a1b25f97
Snehdeep <snehdeepsingh221199@gmail.com>
2021-12-05 00:19:06 +0530

    Assets meta css complete

--- NextJs-Docs/nextjs-blog/pages/index.js
+++ NextJs-Docs/nextjs-blog/pages/index.js
@@ -1,22 +1,20 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main>
-        <h1 className="title">
-          Read{' '}
-          <Link href="/posts/first-post">
-            <a>this page</a>
-          </Link>
-        </h1>
-      </main>
-    </div>  
-      )
+      <section className={utilStyles.headingMd}>
+        <p>Hello There !</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
